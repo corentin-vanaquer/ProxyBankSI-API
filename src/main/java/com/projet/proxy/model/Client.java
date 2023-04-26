417ad57f8d369704d5762a6fc144eb4c2919ad9b
aurore rigault <rigault.aurore@gmail.com>
2023-04-26 10:04:24 +0200

    test git client

--- src/main/java/com/projet/proxy/model/Client.java
+++ src/main/java/com/projet/proxy/model/Client.java
@@ -1,5 +1,14 @@
 package com.projet.proxy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
 }
